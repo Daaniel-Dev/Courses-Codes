6bd2b95ea9e7f5ff8d8c1e1c682505334a33cf2d
Daniel <83424429+not-Daaniel@users.noreply.github.com>
2025-05-19 16:38:46 -0300

    Update Cadastro de alunos.py

--- SENAC - PYTHON/Desafios/Cadastro de alunos.py
+++ SENAC - PYTHON/Desafios/Cadastro de alunos.py
@@ -3,7 +3,7 @@ from Funções.interface import *
 aluno = dict()
 sala = list()
 soma = contador = 0
-titulo('CADASTRO DE TURMA', 9, '=', True)
+titulo('CADASTRO DE TURMA', '-', 35, True)
 while True:
     aluno['nome'] = checar_nome()
     aluno['nota'] = checar_nota()
@@ -16,14 +16,15 @@ while True:
             break
         elif confirmação == 'N':
             tamanho_sala = len(sala)
-            titulo('LISTA DE ALUNOS E NOTAS', 6)
+            titulo('LISTA DE ALUNOS E NOTAS', '-', 35)
             print(f'{'NOME':<30} {'NOTA':<10}')
+            print()
             for estudante in sala:
                 print(f'{estudante['nome']:<30} {nota_formatada(estudante['nota']):<10}')
                 soma = soma + estudante['nota']
             média = soma / tamanho_sala
-            titulo(f'MÉDIA DA TURMA: {nota_formatada(média)}', 8)
-            titulo('ALUNOS ACIMA DA MÉDIA', 7)
+            titulo(f'MÉDIA DA TURMA: {nota_formatada(média)}', '-', 35)
+            titulo('ALUNOS ACIMA DA MÉDIA', '-', 35)
             for estudante in sala:
                 if estudante['nota'] > média:
                     contador += 1
@@ -31,3 +32,4 @@ while True:
             if contador == 0:
                 print('Nenhum aluno está acima da média.')
             exit(0)
+
